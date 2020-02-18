57ff2bcf81352239e549780fddc68f3a9e71feba
Jack Fox <jfox13@nd.edu>
2020-02-18 11:59:13 -0800

    initial code 🚀

---
+++ rocketgit.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+'''rocketgit.py: a script that makes sure your git messages contain a 🚀'''
+__author__ = "Jack Fox"
+__email__ = "jfox13@nd.edu"
+
+import sys
+import subprocess
+
+def has_rocket(lines: str) -> bool:
+    return '🚀' in lines
+
+if __name__ == '__main__':
+    lines = sys.argv
+    lines[0] = 'git'
+
+    if '-m' in lines and lines.index('-m') + 1 < len(lines):
+        comment_index = lines.index('-m') + 1
+        if not has_rocket(lines[comment_index]):
+            lines[comment_index] = '{} 🚀'.format(lines[comment_index])
+
+    process = subprocess.Popen(lines)
